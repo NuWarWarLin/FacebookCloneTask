f31018fd55ca4eff0b4d0414b5869ca4d41d1ddd
Nu War War Lin <nuwarwarlin@pop-os.localdomain>
2020-08-13 00:04:49 +0630

    add post new post create function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,18 @@ class PostsController < ApplicationController
         @posts = Post.all
     end
     
+    def new
+        @post = Post.new
+    end
+    
+    def create
+        @post = Post.new(params.require(:post).permit(:title, :description))
+        if @post.save
+          flash[:notice] = "Posts was created successfully."
+          redirect_to @post
+        else
+          render 'new'
+        end
+     end
 
 end
