ab304d7ae19983201b76c11ba15c1296f46e1853
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-13 08:55:38 +0630

    refactoring the posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
     def index
       @posts = Post.all
     end
@@ -9,7 +9,7 @@ class PostsController < ApplicationController
     end
     
     def create
-        @post = Post.new(params.require(:post).permit(:title, :description))
+        @post = Post.new(post_params)
         if @post.save
           flash[:notice] = "Posts was created successfully."
           redirect_to @post
@@ -19,16 +19,13 @@ class PostsController < ApplicationController
      end
 
     def show
-      @post = Post.find(params[:id])
-      end
+    end
 
     def edit
-      @post = Post.find(params[:id])
     end
 
     def update
-        @post = Post.find(params[:id])
-        if @post.update(params.require(:post).permit(:title, :description))
+        if @post.update(post_params)
            flash[:notice] = "Post was updated successfully."
            redirect_to @post
         else 
@@ -37,9 +34,18 @@ class PostsController < ApplicationController
     end
 
     def destroy
-      @post = Post.find(params[:id])
       @post.destroy
       redirect_to posts_path
+    end
+
+    private
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
+
+    def post_params
+      params.require(:post).permit(:title, :description)
     end
 
 end
