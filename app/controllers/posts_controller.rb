602f17ea5f535b120c2e88ebfe8d65f9b06dc33c
Nu War War Lin <nuwarwarlin@pop-os.localdomain>
2020-08-17 22:52:21 +0630

    restrict actions at controller level

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
     before_action :set_post, only: [:show, :edit, :update, :destroy]
+    before_action :require_user, except: [:show, :index]
+    before_action :require_same_user, only: [:edit, :update, :destroy]
+
     def index
       @posts = Post.paginate(page: params[:page], per_page: 5)
     end
@@ -52,6 +55,13 @@ class PostsController < ApplicationController
 
     def post_params
       params.require(:post).permit(:title, :description)
+    end
+
+    def require_same_user
+      if current_user != @post.user
+        flash[:alert] = "You can only edit or delete your own article"
+        redirect_to @post
+      end
     end
 
 end
