dafe24f8911be3fa84aaa4a620f2c2b91c80dcd2
Nu War War Lin <nuwarwarlin@pop-os.localdomain>
2020-08-17 21:19:45 +0630

    add Authentiction helper method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
 
     def index
         @users = User.paginate(page: params[:page], per_page: 5)
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
     def create
        @user = User.new(user_params)
        if @user.save
+        session[:user_id] = @user.id
         flash[:notice] = "Welcome to the SocialBook #{@user.username}, you have successfully signed up"
         redirect_to posts_path
        else
@@ -19,11 +21,9 @@ class UsersController < ApplicationController
     end
 
     def edit
-       @user = User.find(params[:id])
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
             redirect_to @user
@@ -33,7 +33,6 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = User.find(params[:id])
         @posts = @user.posts.paginate(page: params[:page], per_page: 5)
     end
 
@@ -41,6 +40,9 @@ class UsersController < ApplicationController
     def user_params
         params.require(:user).permit(:username, :email, :password)
     end
-    
+
+    def set_user
+        @user = User.find(params[:id])
+    end
     
 end
