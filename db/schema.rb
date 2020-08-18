b302ecd25c9331f292db2241fb8c2507755e2253
Nu War War Lin <nuwarwarlin@pop-os.localdomain>
2020-08-18 21:19:21 +0630

    delete posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_022747) do
+ActiveRecord::Schema.define(version: 2020_08_18_144749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,14 +22,6 @@ ActiveRecord::Schema.define(version: 2020_08_18_022747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_images_on_user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
